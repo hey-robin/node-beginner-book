a21b44e77d543665ae6ffef46afb6198d21a1d68
Robin Miller <rmillerpdx@gmail.com>
2017-10-15 12:47:18 -0700

    res.write throws error for some reason

--- modules/requestHandlers.js
+++ modules/requestHandlers.js
@@ -1,20 +1,30 @@
-const exec = require('child_process').exec;
 
 function start(res) {
     console.log('request handler "start" was called');
-    var content = 'empty';
 
-    exec('ls -lah', function(error, stdout, stderr) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.write(stdout);
-        res.end();
-    });
+    const body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write(body);
+    res.end();
+
 }
 
 function upload(res) {
     console.log('request handler "upload" was called');
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write(stdout);
+    res.write('hi upload');
     res.end();
 }
 
--- modules/router.js
+++ modules/router.js
@@ -1,12 +1,12 @@
-function route(handle, pathName, res) {
+function route(handle, pathName, response) {
     console.log('routing a request for', pathName);
     if (typeof handle[pathName] === 'function') {
         handle[pathName]();
     } else {
         console.log('No request handler found for', pathName);
-        res.writeHead(404, {'Content-Type': 'text/plain'});
-        res.write('404 Not Found');
-        res.end();
+        response.writeHead(404, {'Content-Type': 'text/plain'});
+        response.write('404 Not Found');
+        response.end();
     }
 }
 

88b1e9714874438a1bc00ab07a918404958c4378
Robin Miller <rmillerpdx@gmail.com>
2017-10-15 12:03:17 -0700

    integrate handle

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const server = require('./modules/server');
 const router = require('./modules/router');
+const requestHandlers = require('./modules/requestHandlers');
 
-server.start(router.route);
+const handle = {};
+handle['/'] = requestHandlers.start;
+handle['/start'] = requestHandlers.start;
+handle['/upload'] = requestHandlers.upload;
+
+server.start(router.route, handle);
--- modules/server.js
+++ modules/server.js
@@ -3,12 +3,12 @@ const url = require('url');
 
 console.log('server module'); 
 
-function start(route) {
+function start(route, handle) {
     function onRequest(req, res) {
         const pathName = url.parse(req.url).pathname;
         console.log('request received for', pathName, 'from', req.url);
 
-        route(pathName);
+        route(handle, pathName);
 
         res.writeHead(200, {'Content-Type': 'text/plain'});
         res.end('hi worlk');
